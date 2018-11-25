49fd3153a5d82c1ff3fbc02d041426a54353e089
TheAllenAnker <theallenanker@gmail.com>
2018-11-25 22:13:01 +0800

    Solve the error in some special conditions and boundary conditions.

--- src/com/allenanker/niuke_advanced_lesson/ClosestBigger.java
+++ src/com/allenanker/niuke_advanced_lesson/ClosestBigger.java
@@ -13,7 +13,7 @@ public class ClosestBigger {
         Stack<Integer> values = new Stack<>();
         for (int i = 0; i < arr.length; i++) {
             int currNum = arr[i];
-            if (!valueMap.containsKey(currNum)) {
+            if (!valueMap.containsKey(currNum) || (valueMap.containsKey(currNum) && !values.contains(currNum))) {
                 ArrayList<Integer> indexes = new ArrayList<>();
                 indexes.add(i);
                 valueMap.put(currNum, indexes);
@@ -33,6 +33,9 @@ public class ClosestBigger {
                         cloestBiggerLeft[index] = Integer.MIN_VALUE;
                     }
                 }
+                if (!values.isEmpty() && values.peek() != curr) {
+                    valueMap.remove(curr);
+                }
             }
             values.push(currNum);
         }
@@ -41,7 +44,7 @@ public class ClosestBigger {
         while (!values.isEmpty()) {
             int curr = values.pop();
             for (int index : valueMap.get(curr)) {
-                cloestBiggerRight[index] = prev;
+                cloestBiggerRight[index] = prev > arr[index] ? prev : Integer.MIN_VALUE;
                 if (!values.isEmpty()) {
                     cloestBiggerLeft[index] = values.peek();
                 } else {
@@ -55,7 +58,7 @@ public class ClosestBigger {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{5, 4, 3, 7};
+        int[] arr = new int[]{5, 4, 4, 3, 7, 3, 3, 4, 4, 7};
         int[][] res = getClosestBigger(arr);
         System.out.println(Arrays.toString(res[0]));
         System.out.println(Arrays.toString(res[1]));
