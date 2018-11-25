8225fe043f0c5be9e44642bfa8e31f4034372d92
TheAllenAnker <theallenanker@gmail.com>
2018-11-25 16:17:25 +0800

    Closest bigger in both directions(Increasing stack).

---
+++ src/com/allenanker/niuke_advanced_lesson/ClosestBigger.java
@@ -0,0 +1,63 @@
+package com.allenanker.niuke_advanced_lesson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ClosestBigger {
+    public static int[][] getClosestBigger(int[] arr) {
+        int[] cloestBiggerLeft = new int[arr.length];
+        int[] cloestBiggerRight = new int[arr.length];
+        HashMap<Integer, ArrayList<Integer>> valueMap = new HashMap<>();
+        Stack<Integer> values = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            int currNum = arr[i];
+            if (!valueMap.containsKey(currNum)) {
+                ArrayList<Integer> indexes = new ArrayList<>();
+                indexes.add(i);
+                valueMap.put(currNum, indexes);
+            } else {
+                ArrayList<Integer> indexes = valueMap.get(currNum);
+                indexes.add(i);
+                valueMap.put(currNum, indexes);
+            }
+
+            while (!values.isEmpty() && values.peek() < currNum) {
+                int curr = values.pop();
+                for (int index : valueMap.get(curr)) {
+                    cloestBiggerRight[index] = currNum;
+                    if (!values.isEmpty()) {
+                        cloestBiggerLeft[index] = values.peek();
+                    } else {
+                        cloestBiggerLeft[index] = Integer.MIN_VALUE;
+                    }
+                }
+            }
+            values.push(currNum);
+        }
+
+        int prev = Integer.MIN_VALUE;
+        while (!values.isEmpty()) {
+            int curr = values.pop();
+            for (int index : valueMap.get(curr)) {
+                cloestBiggerRight[index] = prev;
+                if (!values.isEmpty()) {
+                    cloestBiggerLeft[index] = values.peek();
+                } else {
+                    cloestBiggerLeft[index] = Integer.MIN_VALUE;
+                }
+            }
+            prev = curr;
+        }
+
+        return new int[][]{cloestBiggerLeft, cloestBiggerRight};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 4, 3, 7};
+        int[][] res = getClosestBigger(arr);
+        System.out.println(Arrays.toString(res[0]));
+        System.out.println(Arrays.toString(res[1]));
+    }
+}
