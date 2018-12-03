e6c0a7051f0736bc951ea7356af794b854d87c54
TheAllenAnker <theallenanker@gmail.com>
2018-12-03 09:19:11 +0800

    Find the duplicate in array, the array must not be modified.

--- src/com/allenanker/chapter2/FindDuplicateInArray.java
+++ src/com/allenanker/chapter2/FindDuplicateInArray.java
@@ -2,8 +2,59 @@ package com.allenanker.chapter2;
 
 public class FindDuplicateInArray {
     /**
+     * The same as the below problem, only that the array can't be modified.
+     * The array may not have duplicates.
+     *
+     * @param arr the given array
+     */
+    public static int findDuplicate2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        // the arr values vary from 1 to arr.length
+        int start = 1, end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) >> 1;
+            int count = countRange(arr, start, mid);
+            if (start == end) {
+                // if the count is not bigger than 1, the the array has no duplicate
+                if (count > 1) {
+                    return start;
+                } else {
+                    break;
+                }
+            }
+            // if the number smaller or equal to mid is too big
+            if (count > (mid - start + 1)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int countRange(int[] arr, int start, int end) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= end && arr[i] >= start) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
      * Find one of the duplicates in the arr if it exists, or return Integer.MAX_VALUE.
      * The array has length n, and values in the array are between 0 and n-1.
+     * (The array can be modified.)
      *
      * @param arr the given array
      * @return the duplicate if exists, or return Integer.MAX when there are no duplicate or the input is wrong.
@@ -35,7 +86,9 @@ public class FindDuplicateInArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{2, 3, 1, 0, 2, 5, 3};
-        System.out.println(findDuplicate(arr));
+//        int[] arr = new int[]{2, 3, 1, 0, 2, 5, 3};
+//        System.out.println(findDuplicate(arr));
+        int[] arr1 = new int[]{1, 2, 3, 4, 4, 5};
+        System.out.println(findDuplicate2(arr1));
     }
 }
