81b54e20f6bfc9b73bbbcb123201e77881180aa2
TheAllenAnker <theallenanker@gmail.com>
2018-12-09 11:13:48 +0800

    Fix some bugs in last problem.

--- src/com/allenanker/chapter2/SmallestInRotatedArr.java
+++ src/com/allenanker/chapter2/SmallestInRotatedArr.java
@@ -14,15 +14,25 @@ public class SmallestInRotatedArr {
 
         int start = 0;
         int end = rotatedSortedArr.length - 1;
+        if (rotatedSortedArr[start] < rotatedSortedArr[end]) {
+            return rotatedSortedArr[start];
+        }
         while (start < end) {
             int mid = start + (end - start) / 2;
             if (mid == start) {
                 break;
             }
-            if (rotatedSortedArr[mid] >= rotatedSortedArr[start]) {
+            if (rotatedSortedArr[mid] >= rotatedSortedArr[start] && rotatedSortedArr[mid] != rotatedSortedArr[end]) {
                 start = mid;
-            } else {
+            } else if (rotatedSortedArr[mid] <= rotatedSortedArr[end] && rotatedSortedArr[mid] != rotatedSortedArr[start]) {
                 end = mid;
+            } else {
+                // when the three numbers are equal, just find the smallest in order
+                int min = Integer.MAX_VALUE;
+                for (int i = start; i <= end; i++) {
+                    min = Math.min(rotatedSortedArr[i], min);
+                }
+                return min;
             }
         }
 
@@ -32,5 +42,8 @@ public class SmallestInRotatedArr {
     public static void main(String[] args) {
         int[] rotatedArr = new int[]{2, 1, 1, 1, 1, 2, 2};
         System.out.println(findSmallest(rotatedArr));
+        System.out.println(findSmallest(new int[]{1, 1, 1, 2}));
+        System.out.println(findSmallest(new int[]{1, 0, 1, 1, 1}));
+        System.out.println(findSmallest(new int[]{1, 1, 1, 0, 1}));
     }
 }
