238114edd0d93b4a5dceadbe6030199a62802405
TheAllenAnker <theallenanker@gmail.com>
2018-12-14 10:13:27 +0800

    Solve problem: Print Trees in Lines.

--- src/com/allenanker/chapter4/PrintTreeFromTopToBottom.java
+++ src/com/allenanker/chapter4/PrintTreeFromTopToBottom.java
@@ -3,6 +3,38 @@ package com.allenanker.chapter4;
 import java.util.LinkedList;
 
 public class PrintTreeFromTopToBottom {
+    public static <T> void printTreeByRow(TreeNode<T> head) {
+        if (head == null) {
+            throw new IllegalArgumentException("Invalid parameter head: head of the binary tree cannot be null");
+        }
+
+        LinkedList<TreeNode<T>> nodes = new LinkedList<>();
+        LinkedList<TreeNode<T>> helperQueue = new LinkedList<>();
+        nodes.push(head);
+        while (!nodes.isEmpty()) {
+            while (!nodes.isEmpty()) {
+                helperQueue.offer(nodes.pop());
+            }
+            nodes.clear();
+
+            while (!helperQueue.isEmpty()) {
+                TreeNode<T> curr = helperQueue.pop();
+                if (curr.left != null) {
+                    nodes.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    nodes.offer(curr.right);
+                }
+                System.out.print(curr.val);
+                if (!helperQueue.isEmpty()) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
     /**
      * Print a binary tree from top to bottom.
      */
@@ -34,6 +66,7 @@ public class PrintTreeFromTopToBottom {
         head.right.left = new TreeNode<>(9);
         head.right.right = new TreeNode<>(11);
         printTree(head);
+        printTreeByRow(head);
     }
 }
 
