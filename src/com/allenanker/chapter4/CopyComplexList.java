61a88c0726a2c95a75a49c8aaea0ed08ab8c0a94
TheAllenAnker <theallenanker@gmail.com>
2018-12-15 21:47:18 +0800

    Solve problem: Copy Complex List, without using extra space.

--- src/com/allenanker/chapter4/CopyComplexList.java
+++ src/com/allenanker/chapter4/CopyComplexList.java
@@ -3,6 +3,45 @@ package com.allenanker.chapter4;
 import java.util.HashMap;
 
 public class CopyComplexList {
+    public static <T> ComplexListNode<T> copyComplexList_v2(ComplexListNode<T> head) {
+        if (head == null) {
+            throw new IllegalArgumentException("Invalid parameter head");
+        }
+
+        ComplexListNode<T> curr = head;
+        ComplexListNode<T> next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = new ComplexListNode<>(curr.val);
+            curr.next.next = next;
+            curr = next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        ComplexListNode<T> newHead = new ComplexListNode<>(head.val);
+        ComplexListNode<T> oldHead = new ComplexListNode<>(head.val);
+        ComplexListNode<T> currNew = newHead;
+        ComplexListNode<T> currOld = oldHead;
+        while (curr != null) {
+            currNew.next = curr.next;
+            currOld.next = curr;
+            curr = curr.next.next;
+            currNew = currNew.next;
+            currOld = currOld.next;
+        }
+        currOld.next = null;
+
+        return newHead.next;
+    }
+
     public static <T> ComplexListNode<T> copyComplexList(ComplexListNode<T> head) {
         if (head == null) {
             throw new IllegalArgumentException("Invalid parameter head");
@@ -42,6 +81,7 @@ public class CopyComplexList {
         node4.next = node5;
         node4.random = node2;
         ComplexListNode<Integer> newHead = copyComplexList(head);
+        ComplexListNode<Integer> newHead2 = copyComplexList_v2(head);
     }
 }
 
